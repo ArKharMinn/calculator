adda069e1fdec678528e8de2e48c887b06c9ade1
ArKharMinn <arkharminn.1@gmail.com>
2024-05-17 18:13:40 +0630

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="p-3 rounded shadow-sm bg-dark d-inline-block">
+      <input type="" class="text-end p-2" v-model="inPut" placeholder="0" disabled/>
+      <div class="my-3">
+        <button class="rounded shadow-sm btn btn-secondary">7</button>
+        <button class="rounded mx-3 shadow-sm btn btn-secondary">8</button>
+        <button class="rounded shadow-sm btn btn-secondary">9</button>
+        <button class="rounded ms-3 shadow-sm btn btn-secondary">+</button>
+      </div>
+      <div>
+        <button class="rounded shadow-sm btn btn-secondary">4</button>
+        <button class="rounded mx-3 shadow-sm btn btn-secondary">5</button>
+        <button class="rounded shadow-sm btn btn-secondary">6</button>
+        <button class="rounded ms-3 shadow-sm btn btn-secondary">-</button>
+      </div>
+      <div class="my-3">
+        <button class="rounded shadow-sm btn btn-secondary">1</button>
+        <button class="rounded mx-3 shadow-sm btn btn-secondary">2</button>
+        <button class="rounded shadow-sm btn btn-secondary">3</button>
+        <button class="rounded ms-3 shadow-sm btn btn-secondary">x</button>
+      </div>
+      <div>
+        <button @click="clear()" class="rounded shadow-sm btn btn-danger">AC</button>
+        <button class="rounded mx-2 shadow-sm btn btn-secondary">0</button>
+        <button class="rounded shadow-sm btn btn-secondary">=</button>
+        <button class="rounded ms-3 shadow-sm btn btn-secondary">/</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        inPut : 0
+      }
+    },
+    methods: {
+      clear () {
+       this.inPut = 0;
+      },
+      operation(){
+        
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
